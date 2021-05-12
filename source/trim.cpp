42e8f35ff7c313c0f944c6a5ef78afc8ab4924e4
Worthy Persimmon <wpersimm@va-g3.kzn.21-school.ru>
2021-05-12 13:35:05 +0300

    trimV2

--- source/trim.cpp
+++ source/trim.cpp
@@ -1,19 +1,21 @@
 // trim from end of string (right)
-inline std::string& rtrim(std::string& s, char* t)
+#include <../header.h>
+
+std::string& rtrim(std::string& s, char* t)
 {
     s.erase(s.find_last_not_of(t) + 1);
     return s;
 }
 
 // trim from beginning of string (left)
-inline std::string& ltrim(std::string& s, char* t)
+std::string& ltrim(std::string& s, char* t)
 {
     s.erase(0, s.find_first_not_of(t));
     return s;
 }
 
 // trim from both ends of string (right then left)
-inline std::string& trim(std::string& s, char* t)
+std::string& trim(std::string& s, char* t)
 {
     return ltrim(rtrim(s, t), t);
 }
