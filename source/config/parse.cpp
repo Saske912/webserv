04b06580bd3873ea7f30ae2d6a92792003b96da9
itressa <itressa@student.21-school.ru>
2021-07-02 11:12:06 +0300

    added config file validation

--- source/config/parse.cpp
+++ source/config/parse.cpp
@@ -9,33 +9,64 @@
 #include "ParseResult.hpp"
 #include "config.hpp"
 
+bool is_config_file_invalid(const char *filename) {
+    struct stat st;
+    if (-1 == stat(filename, &st)) {
+        return true;
+    }
+    if (st.st_mode & S_IFMT & S_IFDIR) {
+        errno = EISDIR;
+        return true;
+    }
+    return false;
+}
+
+std::size_t check_tokens_unexpected(std::vector<Token> &token) {
+    std::size_t                       found = 0;
+    for (std::vector<Token>::iterator it    = token.begin();
+         it != token.end(); ++it) {
+        if (it->type == Token::UNEXPECTED) {
+            ErrorNode *err = ErrorNode::getSyntaxError(*it, "Unexpected symbol " + it->text());
+            std::cout << err;
+            delete err;
+            ++found;
+        }
+    }
+    return found;
+}
+
 config parse(int fd, const char *filename) {
     std::vector<Token> tokens = Lexer(fd, filename).make_tokens();
-    ParseResult        ast    = Parser(tokens).parse();
+    if (std::size_t errors = check_tokens_unexpected(tokens)) {
+        if (errors + 1 >= tokens.size() - tokens.size() / 10) {
+            std::cout << "Config file seem to be binary" << std::endl;
+        }
+        exit(32);
+    }
+    ParseResult ast = Parser(tokens).parse();
     if (ast.error) {
         std::cout << ast.error << std::endl;
-        exit(1);
+        delete ast.error;
+        exit(33);
     }
     config result;
     Interpreter(result).visit(ast.node);
     delete ast.node;
-    delete ast.error;
     return result;
 }
 
-config * parse( const char *filename) {
+config *parse(const char *filename) {
     std::list<server>::iterator it;
     std::list<server>::iterator ite;
-    config *result = new config;
-    int fd = open(filename, O_RDONLY);
-    if (-1 == fd) {
-        error_exit(std::string("Error opening config file '") + filename + "'");
+    config                      *result = new config;
+    if (is_config_file_invalid(filename)) {
+        error_exit(std::string("Invalid config file '") + filename + "'");
     }
+    int fd = open(filename, O_RDONLY);
     *result = parse(fd, filename);
-    it = result->getServers().begin();
+    it  = result->getServers().begin();
     ite = result->getServers().end();
-    while (it != ite)
-    {
+    while (it != ite) {
         it->setConf(result);
         it++;
     }
